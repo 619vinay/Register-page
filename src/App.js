db41ef314b1e448580c3d5b3061615ac66fca0af
619vinay <kanakamvinay47630@gmail.com>
2023-09-29 19:21:02 +0530

    register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './index.css'
+import logo from './Razor.jpg'
+import axios from 'axios'
 
-function App() {
+
+
+
+const App = () => {
+  const [data,setdata] = useState({
+    username:"",
+    email:"",
+    password:"",
+    conpassword:"",
+  })
+  const {username,email,password,conpassword} =data;
+  const handler = e =>{
+    setdata({...data,[e.target.name]:e.target.value})
+  }
+
+  const submit = e =>{
+    e.preventDefault()
+   if (username.length <= 5){
+    alert("username must be at least 5 character")
+   }
+   else if (password !== conpassword){
+    alert("passwords are not matching")
+   }
+   else if (password === "" && conpassword === ""){
+    alert("passwords are not matching")
+   }
+   else{
+     axios.post('https://form-validatiom-testing-default-rtdb.firebaseio.com/register.json',data).then( () => alert("submitted successfully")).catch( () => alert("submitted failed"))
+   }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+    <div className='main'>
+      <h1 className='text'>Testing validation form.</h1>
+       <img src={logo} alt=''></img>
+    
+      <form onSubmit={submit}>
+        <input type='text' placeholder='username' value={username} name='username' onChange={handler}></input><br/><br/>
+        <input type='text' placeholder='email' value={email} name='email' onChange={handler} required></input><br/><br/>
+        <input type='password' placeholder='password' value={password} name='password' onChange={handler}></input><br/><br/>
+        <input type='password' placeholder='Confirm password' value={conpassword} name='conpassword' onChange={handler}></input><br/><br/>
+        <input type='submit' className='btn'></input>
+     </form>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/myapp.js
@@ -0,0 +1,13 @@
+import React,{component} from 'react';
+class App extends component {
+    render (){
+        var mystyle ={
+            fontsize:60,
+            color:red,
+        }
+        return (
+            <h1>hello world</h1>
+        );
+    }
+}
+export default App;
